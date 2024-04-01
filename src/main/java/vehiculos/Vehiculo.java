4515f0444162163452ef6becee40b5736168d6a0
Johan Ramirez <159493340+joramirezma@users.noreply.github.com>
2024-04-01 13:05:50 -0500

    a

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -1,5 +1,27 @@
 package vehiculos;
 
 public class Vehiculo {
-
+	protected String placa;
+	protected int puertas;
+	protected int velocidadMaxima;
+	protected String nombre;
+	protected int precio;
+	protected int peso;
+	protected String traccion;
+	protected Fabricante fabricante;
+	private static int cantidadVehiculos;
+	
+	public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso,
+			String traccion, Fabricante fabricante) {
+		this.placa = placa;
+		this.puertas = puertas;
+		this.velocidadMaxima = velocidadMaxima;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.peso = peso;
+		this.traccion = traccion;
+		this.fabricante = fabricante;
+	}
+	
+	
 }
