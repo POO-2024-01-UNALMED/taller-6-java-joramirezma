29b616538f10c371428d39da4744af7dd763980b
Johan Ramirez <159493340+joramirezma@users.noreply.github.com>
2024-04-01 13:37:41 -0500

    d

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -3,6 +3,12 @@ package vehiculos;
 public class Fabricante {
 	String nombre;
 	Pais pais;
+	
+	public Fabricante(String nombre, Pais pais) {
+		this.nombre = nombre;
+		this.pais = pais;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -2,6 +2,10 @@ package vehiculos;
 
 public class Pais {
 	String nombre;
+		
+	public Pais(String nombre) {
+		this.nombre = nombre;
+	}
 
 	public String getNombre() {
 		return nombre;
